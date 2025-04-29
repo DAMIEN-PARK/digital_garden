25fd21f3af9c7953cf87cb69471a873694548b36
DAMIEN-PARK <damien.park.personal@gmail.com>
2025-04-29 22:40:46 +0900

    vault backup: 2025-04-29 22:40:46

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Source": "https://github.com/DAMIEN-PARK/digital_garden",
+      "Home": "https://damiengarden.vercel.app/",
     },
   }),
 }
